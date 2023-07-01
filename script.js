6c5f6ab163e49a38aad04a85f8dd201c03a6d45e
samuel <samuelkariuki1@student.moringaschool.com>
2023-07-01 11:27:54 +0300

    adding fetch

---
+++ script.js
@@ -0,0 +1,17 @@
+//fetching the movie details 
+
+function flatironMovies (movies) {
+
+    fetch("http://localhost:3000/films")
+
+    .then(res => res.json())
+
+    .then(movies=>{
+        movies.forEach((films)=>{
+
+         const moviesquad = document.getElementById("")
+
+        })
+    })
+
+    }
